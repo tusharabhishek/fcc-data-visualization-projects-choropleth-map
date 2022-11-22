5141db60337b0f1cdb19fd291ac776d15d4693ec
Tushar Abhishek <tusharabhishek8@gmail.com>
2022-11-22 20:54:05 +0530

    Added map data

--- src/scripts/graph.js
+++ src/scripts/graph.js
@@ -1,8 +1,37 @@
 import * as d3 from 'd3';
+import * as topojsonClient from 'topojson-client';
 
 function createGraph(educationData, countyData) {
   console.log(educationData);
   console.log(countyData);
+
+  const mapData = topojsonClient.feature(countyData, 'counties');
+  console.log(mapData);
+
+  const graphDim = {
+    w: 800,
+    h: 600,
+    p: 50
+  };
+
+  const graph = d3.select('svg')
+    .attr('width', graphDim.w)
+    .attr('height', graphDim.h);
+  
+  const geoGenerator = d3.geoPath();
+
+  graph.selectAll('path')
+    .data(mapData.features)
+    .join('path')
+    .attr('d', geoGenerator)
+    .attr('fill', d => randomColor());
+}
+
+function randomColor() {
+  function randomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  return `rgb(${randomInt(256)}, ${randomInt(256)}, ${randomInt(256)})`;
 }
 
 export default createGraph;
